b34737df68dbaa472bb9308509fecf45411b3a44
PerilsEdge <114953514+PerilsEdge@users.noreply.github.com>
2023-01-30 18:20:08 -0700

    Add files via upload

--- MovementFS.cpp
+++ MovementFS.cpp
@@ -11,7 +11,8 @@ using pros::E_CONTROLLER_DIGITAL_L1;
 using pros::E_CONTROLLER_DIGITAL_L2;
 using pros::E_CONTROLLER_DIGITAL_A;
 using pros::E_CONTROLLER_DIGITAL_B;
-using pros::E_CONTROLLER_DIGITAL_UP;
+//using pros::E_CONTROLLER_DIGITAL_UP;
+//using pros::E_CONTROLLER_DIGITAL_DOWN;
 using pros::E_MOTOR_ENCODER_DEGREES;
 using pros::E_MOTOR_GEARSET_36;
 using pros::E_MOTOR_GEARSET_18;
@@ -32,44 +33,65 @@ pros::Motor LF(10, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
 pros::Motor RF (8, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
 pros::Motor LB (16, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
 pros::Motor RB (11, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
-pros::Motor Conveyer (5, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+pros::Motor Conveyor (1, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
 pros::Motor Roller (19, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-pros::Motor Spool ( 1, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+pros::Motor Spool ( 5, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
 
 pros::Controller master (pros::E_CONTROLLER_MASTER);
 
 void autonomous() {
+  bool yes = true;
+  while (yes) {
+        //move back
+        RB.move_velocity(-75);
+        LB.move_velocity(-75);
+        pros::delay(200);
+        RB.move_velocity(0);
+        LB.move_velocity(0);
 
+        //roller upward
+        Roller.move_velocity(-95);
+        pros::delay(780);
+        Roller.move_velocity(0);
+/*
+        //move forward
+        RB.move_velocity(75);
+        LB.move_velocity(75);
+        pros::delay(150);
+        RB.move_velocity(0);
+
+        //turn 90* right
+        RF.move_velocity(-75);
+        pros::delay(820);
+
+        //move forward
+        RF.move_velocity(100);
+        LF.move_velocity(100);
+        RB.move_velocity(100);
+        LB.move_velocity(100);
+        pros::delay(2800);
+        RF.move_velocity(0);
+        LF.move_velocity(0);
+        RB.move_velocity(0);
+        LB.move_velocity(0);
+
+        Conveyer.move_velocity(-200);
+    */    
+        yes = false;
+      }
 }
 
-void resetposition() {
-  LB.tare_position();
-  LF.tare_position();
-  RB.tare_position();
-  RF.tare_position();
-  }
-int step1=0;
+
 void opcontrol() 
 {
 
     while (true) 
-    {
-      //Autonomous when ^ is pressed
-      while (master.get_digital(E_CONTROLLER_DIGITAL_UP)) {
-        RB.move_velocity(-75);
-        LB.move_velocity(-75);
-        Roller.move_velocity(-75);
-        pros::delay(1000);
-        RB.move_velocity(0);
-        LB.move_velocity(0);
-        Roller.move_velocity(0);
-      }
-    
+    {    
     // Y-axis control(acceleration)
-    RF.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * 250 / 127);
-    LF.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * 250 / 127);
-    RB.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * 250 / 127);
-    LB.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * 250 / 127);
+    RF.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * -250 / 127);
+    LF.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * -250 / 127);
+    RB.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * -250 / 127);
+    LB.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * -250 / 127);
 
     // X-axis control(turning)
     if (master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X) <= -10) {
@@ -80,13 +102,13 @@ void opcontrol()
       LB.move_velocity(master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X) * 300 / 127);
     }
 
-    // Conveyer Movement
+    // Expansion Spool
     if (master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
-      Conveyer.move_velocity(-550);
+      Spool.move_velocity(-450);
     } else if (master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
-      Conveyer.move_velocity(550);
+      Spool.move_velocity(450);
     } else {
-      Conveyer.move_velocity(0);
+      Spool.move_velocity(0);
     }
 
     //Roller Movement
@@ -100,13 +122,13 @@ void opcontrol()
       Roller.move_velocity(0);
     }
 
-    //Expansion Spool
+    // Conveyor Movement
     if (master.get_digital(E_CONTROLLER_DIGITAL_A)) {
-      Spool.move_velocity(400);
+      Conveyor.move_velocity(550);
     } else if (master.get_digital(E_CONTROLLER_DIGITAL_B)) {
-      Spool.move_velocity(-400);
+      Conveyor.move_velocity(-550);
     } else {
-      Spool.move_velocity(0);
+      Conveyor.move_velocity(0);
     }
 
       pros::delay(20);
